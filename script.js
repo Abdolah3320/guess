eb6133a1772f4550db3c39a2f03f1f6d7eba1929
Abdolah3320 <130370108+Abdolah3320@users.noreply.github.com>
2024-01-08 15:40:22 +0200

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-// 'use strict';
-// const secretNumber = Math.floor(Math.random() * 20 + 1);
-// document.querySelector('.number').textContent = '?';
-// let attempts = 20;
-// document.querySelector('.score').textContent = attempts;
-// let hightScore = 0
-// document.querySelector('.highscore').textContent = hightScore;
-// document.querySelector('.check').addEventListener('click', function () {
-//   const guess = Number(document.querySelector('.guess').value);
-//   if (!guess) {
-//     document.querySelector('.message').textContent = '🤡 Not a number';
-//   } else if (guess > secretNumber) {
-//     document.querySelector('.message').textContent = 'Too high';
-//     attempts = Math.max(attempts - 1, 0);
-//   } else if (guess < secretNumber) {
-//     document.querySelector('.message').textContent = 'Too low';
-//     attempts = Math.max(attempts - 1, 0);
-//   } else if (guess === secretNumber) {
-//     document.querySelector('.message').textContent = '🎉 Correct guess!';
-//     document.querySelector('body').style.backgroundColor = '#3cb043';
-//     document.querySelector('.number').textContent = secretNumber;
-//   }
-//   if(guess === secretNumber){
-//     document.querySelector('.highscore').textContent=secretNumber
-//   }else if (secretNumber > hightScore){
-//     hightScore =hightScore
-//   }else if(secretNumber<hightScore){
-//     hightScore=secretNumber
-//   }
 
-//   document.querySelector('.score').textContent = attempts;
-
-//   if (attempts === 0) {
-//     document.querySelector('.message').textContent = 'Game Over. Try again!';
-//   }
-// });
 'use strict';
 let secretNumber, attempts, hightScore;
 
